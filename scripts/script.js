448e16084a6a5069bfa4810c559c99e4bbbdb48d
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-01-08 21:41:36 -0300

    implementação do botão de finalizar pedido

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-function selecionarPrato(pratoClickado){ //pegar o prato que foi clickado e trazer para o JS (usando o this)
+let tituloPrato;
+let tituloBebida;
+let tituloSobremesa;
+
+function selecionarPrato(pratoClickado, seletorTitulo){ //pegar o prato que foi clickado e trazer para o JS (usando o this)
 
     //pegar o elemento que foi selecionado anteriormente
     const pratoSelecionadoAnteriormente = document.querySelector('.pratos .selecionado');
@@ -11,9 +15,17 @@ function selecionarPrato(pratoClickado){ //pegar o prato que foi clickado e traz
 
     //adiconar a classe selecionado no elemento que foi clickado
     pratoClickado.classList.add('selecionado');
+
+    //pegar titulo do prato
+    const elementoTitulo = document.querySelector(seletorTitulo+' .titulo');
+
+    //pegar o texto que está no elemento
+    tituloPrato = elementoTitulo.innerHTML;
+
+    ativarBotaoFecharPedido();
 }
 
-function selecionarBebida(bebidaClickada){ //pegar a bebida que foi clickada (this)
+function selecionarBebida(bebidaClickada, seletorTitulo){ //pegar a bebida que foi clickada (this)
 
     //pegar a bebida que foi selecionado anteriormente
     const bebidaSelecionadaAnteriormente = document.querySelector('.bebidas .selecionado');
@@ -26,9 +38,17 @@ function selecionarBebida(bebidaClickada){ //pegar a bebida que foi clickada (th
     
     //marcar como selecionada, adicionando a classe 'selecionado'
     bebidaClickada.classList.add('selecionado');
+
+    //pegar o titulo
+    const elementoTitulo = document.querySelector(seletorTitulo+' .titulo');
+
+    //pegar o texto que está no elemento
+    tituloBebida = elementoTitulo.innerHTML;
+
+    ativarBotaoFecharPedido();
 }
 
-function selecionarSobremesa(sobremesaClickada){ //pegar a sobremesa que foi clickada (this)
+function selecionarSobremesa(sobremesaClickada, seletorTitulo){ //pegar a sobremesa que foi clickada (this)
 
     //pegar a sobremesa que foi selecionado anteriormente
     const sobremesaSelecionadaAnteriormente = document.querySelector('.sobremesas .selecionado');
@@ -41,4 +61,30 @@ function selecionarSobremesa(sobremesaClickada){ //pegar a sobremesa que foi cli
     
     //marcar como selecionada, adicionando a classe 'selecionado'
     sobremesaClickada.classList.add('selecionado');
+
+    //pegar o titulo
+    const elementoTitulo = document.querySelector(seletorTitulo+' .titulo');
+
+    //pegar o texto que está no elemento
+    tituloSobremesa = elementoTitulo.innerHTML;
+
+    ativarBotaoFecharPedido();
+}
+
+function ativarBotaoFecharPedido(){
+    //se o prato estiver selecionado
+    if(tituloPrato !== undefined){
+        //se a bebida estiver selecionada
+        if(tituloBebida !== undefined) {
+            //se a sobremesa estiver selecionada
+            if (tituloSobremesa !== undefined){                
+                // pegar o botão e trazer para o javascript
+                const botao = document.querySelector('.fazer-pedido');
+                // adcionar a classe selecionado nesse botao adicionando a classe 'ativo'
+                botao.classList.add('ativo');
+                // trocar o texto do botão para 'Fechar Pedido'
+                botao.innerHTML = 'Fechar pedido';
+            }
+        }
+    }
 }
